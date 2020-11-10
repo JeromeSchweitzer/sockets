f3fd7195a998522128f9caa010bea920ec05f68e
Jerome Schweitzer <jeromeschweitzer@Jeromes-MacBook-Pro.local>
2020-11-10 15:06:32 -0500

    some changes

--- client.py
+++ client.py
@@ -4,10 +4,19 @@ HOST = '127.0.0.1'
 PORT = 8888
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((HOST, PORT))
 
-while True:
-    outgoing_msg = input("enter input: ")
-    client_socket.send(bytes(outgoing_msg, "utf-8"))
-    if outgoing_msg == "goodbye":
-        break
+with client_socket:
+    client_socket.connect((HOST, PORT))
+
+    while True:
+        outgoing_msg = input("Enter message for server: ")
+        client_socket.send(bytes(outgoing_msg, "utf-8"))
+        if outgoing_msg == "goodbye":
+            print("You (client) ended connection")
+            break
+
+        incoming_data = client_socket.recv(1024)
+        if not incoming_data:
+            print("Server ended connection")
+            break
+        print("received", repr(incoming_data))
--- server.py
+++ server.py
@@ -3,23 +3,32 @@ import socket
 
 def handle_client(client_conn, client_addr):
     print('Connected by', client_addr)
-    while True:
-        incoming_data = client_conn.recv(1024)
-        if not incoming_data:
-            client_conn.close()
-            print("client disconnected")
-            break
-        print("received", repr(incoming_data))
+
+    with client_conn:
+        while True:
+            incoming_data = client_conn.recv(1024)
+            if not incoming_data:
+                print("Client ended connection")
+                break
+            print("received", incoming_data)
+            
+            outgoing_msg = input("Enter message for client: ")
+            client_conn.send(bytes(outgoing_msg, "utf-8"))
+            if outgoing_msg == "goodbye":
+                print("You (server) ended connection")
+                break
 
 
 HOST = '127.0.0.1'
 PORT = 8888
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((HOST, PORT))
 
-while True:
-    server_socket.listen()
-    client_conn, client_addr = server_socket.accept()
-    handle_client(client_conn, client_addr)
+with server_socket:
+    server_socket.bind((HOST, PORT))
+
+    while True:
+        server_socket.listen()
+        client_conn, client_addr = server_socket.accept()
+        handle_client(client_conn, client_addr)
 
