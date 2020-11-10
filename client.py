671d16ec62f0b16b73110aa6816a7b0297b35026
Jerome Schweitzer <jeromeschweitzer@Jeromes-MacBook-Pro.local>
2020-11-10 17:22:18 -0500

    updating

--- client.py
+++ client.py
@@ -1,13 +1,31 @@
 import socket
 
-HOST = '127.0.0.1'
-PORT = 8888
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((HOST, PORT))
+HOST="127.0.0.1"
+PORT=8888
+EXIT_MESSAGE="goodbye"
 
-while True:
-    outgoing_msg = input("enter input: ")
-    client_socket.send(bytes(outgoing_msg, "utf-8"))
-    if outgoing_msg == "goodbye":
-        break
+
+def start_client(host_ip, port_addr):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    with client_socket:
+        client_socket.connect((host_ip, port_addr))
+
+        while True:
+            outgoing_msg = input("Enter message for server: ")
+            outgoing_data = bytes(outgoing_msg, "utf-8")
+            client_socket.send(outgoing_data)
+            if outgoing_msg == EXIT_MESSAGE:
+                print("You ended the connection.")
+                break
+
+            incoming_data = client_socket.recv(1024)
+            if not incoming_data or incoming_data.decode("utf-8") == EXIT_MESSAGE:
+                print("Server ended the connection.\n")
+                break
+            print("Received: \"%s\"" % incoming_data.decode("utf-8"))
+
+
+if __name__ == "__main__":
+    start_client(host_ip=HOST, port_addr=PORT)
--- server.py
+++ server.py
@@ -1,25 +1,41 @@
 import socket
 
 
+HOST="127.0.0.1"
+PORT=8888
+EXIT_MESSAGE="goodbye"
+
+
+def start_server(host_ip, port_addr):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    with server_socket:
+        server_socket.bind((host_ip, port_addr))
+        while True:
+            server_socket.listen()
+            client_conn, client_addr = server_socket.accept()
+            handle_client(client_conn, client_addr)
+
+
 def handle_client(client_conn, client_addr):
-    print('Connected by', client_addr)
-    while True:
-        incoming_data = client_conn.recv(1024)
-        if not incoming_data:
-            client_conn.close()
-            print("client disconnected")
-            break
-        print("received", repr(incoming_data))
+    print(f"Connected by {client_addr}")
+
+    with client_conn:
+        while True:
+            incoming_data = client_conn.recv(1024)
+            if not incoming_data or incoming_data.decode("utf-8") == EXIT_MESSAGE:
+                print("Client ended the connection.")
+                break
+
+            print("Received: \"%s\"" % incoming_data.decode("utf-8"))
+
+            outgoing_msg = input("Send to client: ")
+            outgoing_data = bytes(outgoing_msg, "utf-8")
+            client_conn.send(outgoing_data)
+            if outgoing_msg == EXIT_MESSAGE:
+                print("You ended the connection.\n")
+                break
 
 
-HOST = '127.0.0.1'
-PORT = 8888
-
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((HOST, PORT))
-
-while True:
-    server_socket.listen()
-    client_conn, client_addr = server_socket.accept()
-    handle_client(client_conn, client_addr)
-
+if __name__ == "__main__":
+    start_server(host_ip=HOST, port_addr=PORT)
