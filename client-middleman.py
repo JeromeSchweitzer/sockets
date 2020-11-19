592c487711cfba1276c0d8a0ea1b21ddbd002e92
Jerome Schweitzer <jeromeschweitzer@pal-nat186-142-16.itap.purdue.edu>
2020-11-19 18:32:39 -0500

    more changes

--- client-middleman.py
+++ client-middleman.py
@@ -37,7 +37,7 @@ def receive_coordinates(client_socket, board):
     incoming_data = client_socket.recv(1024)
     if not incoming_data or incoming_data.decode("utf-8") == EXIT_MESSAGE:
         print(f"Server connection closed.")
-        break
+        return False
     coords_string = incoming_data.decode("utf-8")
     client_row = int(coords_string[0])
     client_col = int(coords_string[1])
