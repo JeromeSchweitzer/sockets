838d97d3413530bc4b5b7f524f9c6490ebecb8c9
Jerome Schweitzer <jeromeschweitzer@Jeromes-MacBook-Pro.local>
2020-11-18 21:42:50 -0500

    initial commit

--- client.py
+++ client.py
@@ -1,9 +1,29 @@
 import socket
+import threading
 
 
 HOST="127.0.0.1"
-PORT=8888
+PORT=9999
 EXIT_MESSAGE="goodbye"
+
+
+def send_data(client_socket):
+    while True:
+        outgoing_msg = input("Enter message: ")
+        outgoing_data = bytes(outgoing_msg, "utf-8")
+        client_socket.send(outgoing_data)
+        if outgoing_msg == EXIT_MESSAGE:
+            print("You ended the connection.")
+            break
+
+
+def receive_data(client_socket):
+    while True:
+        incoming_data = client_socket.recv(1024)
+        if not incoming_data or incoming_data.decode("utf-8") == EXIT_MESSAGE:
+            print("Server ended the connection.\n")
+            break
+        print("Received: \"%s\"" % incoming_data.decode("utf-8"))
 
 
 def start_client(host_ip, port_addr):
@@ -12,19 +32,11 @@ def start_client(host_ip, port_addr):
     with client_socket:
         client_socket.connect((host_ip, port_addr))
 
-        while True:
-            outgoing_msg = input("Enter message for server: ")
-            outgoing_data = bytes(outgoing_msg, "utf-8")
-            client_socket.send(outgoing_data)
-            if outgoing_msg == EXIT_MESSAGE:
-                print("You ended the connection.")
-                break
+        send_thread = threading.Thread(target=send_data, args=(client_socket))
+        receive_thread = threading.Thread(target=receive_data, args=(client_socket))
 
-            incoming_data = client_socket.recv(1024)
-            if not incoming_data or incoming_data.decode("utf-8") == EXIT_MESSAGE:
-                print("Server ended the connection.\n")
-                break
-            print("Received: \"%s\"" % incoming_data.decode("utf-8"))
+        send_thread.start()
+        receive_thread.start()
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -1,9 +1,26 @@
 import socket
+import threading
 
 
 HOST="127.0.0.1"
 PORT=8888
 EXIT_MESSAGE="goodbye"
+CLIENTS={}
+
+
+def send_to_all(incoming_data, sending_addr):
+    for addr, conn in CLIENTS:
+        if addr not sending_addr:
+            conn.send(incoming_data)
+
+
+def receive_data(client_conn, client_addr):
+    while True:
+        incoming_data = client_conn.recv(1024)
+        if not incoming_data or incoming_data.decode("utf-8") == EXIT_MESSAGE:
+            print("Client ended the connection.")
+            break
+        send_to_all(incoming_data, sending_addr=client_addr)
 
 
 def start_server(host_ip, port_addr):
@@ -14,27 +31,31 @@ def start_server(host_ip, port_addr):
         while True:
             server_socket.listen()
             client_conn, client_addr = server_socket.accept()
-            handle_client(client_conn, client_addr)
+            CLIENTS[client_addr] = client_conn
+            client_thread = threading.Thread(target=receive_data, args=(client_conn, client_addr))
+            client_thread.start()
+            #handle_client(client_conn, client_addr)
 
 
-def handle_client(client_conn, client_addr):
-    print(f"Connected by {client_addr}")
+# def handle_client(client_conn, client_addr):
+#     print(f"Connected by {client_addr}")
 
-    with client_conn:
-        while True:
-            incoming_data = client_conn.recv(1024)
-            if not incoming_data or incoming_data.decode("utf-8") == EXIT_MESSAGE:
-                print("Client ended the connection.")
-                break
+#     with client_conn:
 
-            print("Received: \"%s\"" % incoming_data.decode("utf-8"))
+        # while True:
+            # incoming_data = client_conn.recv(1024)
+            # if not incoming_data or incoming_data.decode("utf-8") == EXIT_MESSAGE:
+            #     print("Client ended the connection.")
+            #     break
 
-            outgoing_msg = input("Send to client: ")
-            outgoing_data = bytes(outgoing_msg, "utf-8")
-            client_conn.send(outgoing_data)
-            if outgoing_msg == EXIT_MESSAGE:
-                print("You ended the connection.\n")
-                break
+            # print("Received: \"%s\"" % incoming_data.decode("utf-8"))
+
+        #     outgoing_msg = input("Send to client: ")
+        #     outgoing_data = bytes(outgoing_msg, "utf-8")
+        #     client_conn.send(outgoing_data)
+        #     if outgoing_msg == EXIT_MESSAGE:
+        #         print("You ended the connection.\n")
+        #         break
 
 
 if __name__ == "__main__":
